247e4571220d6411ae7d0597f684f7363cfac49c
hideo-yasui <yasumihosi@gmail.com>
2018-12-06 14:16:26 +0900

    favicon追加

--- resources/views/layouts/start.blade.php
+++ resources/views/layouts/start.blade.php
@@ -6,6 +6,8 @@
 <!-- layouts.start start-->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="{{asset('favicon.png')}}" type="image/ico">
+<link rel="apple-touch-icon" href="{{asset('apple-touch-icon.png')}}">
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <!-- Scripts -->
