7562b3d63b5b1d4829efeae9c5598109f08b7820
hideo-yasui <yasumihosi@gmail.com>
2019-04-18 11:16:19 +0900

    休み連絡

--- resources/views/calendars/rest.blade.php
+++ resources/views/calendars/rest.blade.php
@@ -1,13 +1,14 @@
 @component('calendars.page', ['item' => $item, 'fields' => $fields, 'domain' => $domain])
   @slot('page_message')
     @if(strtotime(date('Y/m/d H:i:s')) >= strtotime($item["date"].' 09:00:00'))
-      {{-- 授業当日9時を過ぎたら休み連絡はできない --}}
+      {{-- 授業当日9時を過ぎたら休み連絡はできない
       <div class="col-12 col-lg-12 col-md-12 mb-1">
         <h4 class="text-danger">授業当日AM9:00以降の休み連絡はできません。</h4>
       </div>
+      --}}
     @else
-      この授業予定をお休みしますか？
     @endif
+    この授業予定をお休みしますか？
   @endslot
   @slot('forms')
   <form method="POST" action="/calendars/{{$item['id']}}/rest">
@@ -17,6 +18,7 @@
       <input type="hidden" value="{{$student_id}}" name="student_id" />
     @endif
   <div class="row">
+    {{--
     @if(strtotime(date('Y/m/d H:i:s')) >= strtotime($item["date"].' 09:00:00'))
     <div class="col-12 mb-1">
         <button type="reset" class="btn btn-secondary btn-block">
@@ -24,6 +26,8 @@
         </button>
     </div>
     @else
+    @endif
+    --}}
     <div class="col-12 col-lg-6 col-md-6 mb-1">
         <button type="button" class="btn btn-submit btn-danger btn-block"  accesskey="{{$domain}}_action">
           <i class="fa fa-envelope mr-1"></i>
@@ -35,7 +39,6 @@
             閉じる
         </button>
     </div>
-    @endif
   </div>
   </form>
   @endslot
