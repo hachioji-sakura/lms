146141bf8bc5be40a22753c32f7d5efe7f565408
hideo-yasui <yasumihosi@gmail.com>
2019-06-04 11:16:22 +0900

    科目→場所に変更

--- resources/views/teachers/calendar.blade.php
+++ resources/views/teachers/calendar.blade.php
@@ -91,7 +91,7 @@
           eventRender: function(event, element) {
             var title = '授業追加';
             if(event['student_name']){
-              title = event['student_name']+'('+event['subject']+')<br>'+event['start_hour_minute']+'-'+event['end_hour_minute'];
+              title = event['student_name']+'('+event['place_name']+')<br>'+event['start_hour_minute']+'-'+event['end_hour_minute'];
             }
             event_render(event, element, title);
           },
