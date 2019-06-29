467d7d726884941dd2f933ef6d7a03ae6fbcf5f6
hideo-yasui <yasumihosi@gmail.com>
2019-06-29 11:50:59 +0900

    予定確認は生徒・保護者しかできなくする

--- resources/views/calendars/fix.blade.php
+++ resources/views/calendars/fix.blade.php
@@ -1,23 +1,27 @@
 @component('calendars.page', ['item' => $item, 'fields' => $fields, 'domain' => $domain, 'action' => $action, 'user'=>$user])
   @slot('page_message')
-  この授業予定の確認連絡をしますか？
+  @if($user->role=="parent" || $user->role=="student")
+    この授業予定の確認連絡をしますか？
+  @endif
   @endslot
   @slot('forms')
   <form method="POST" action="/calendars/{{$item['id']}}" id="_form">
     @csrf
     @method('PUT')
-    <div class="row">
-      @component('calendars.forms.fix_form', ['item' => $item, 'user'=>$user]) @endcomponent
-      @component('calendars.forms.target_member', ['item' => $item, 'user'=>$user, 'status'=>'fix', 'student_id' => $student_id]) @endcomponent
-    </div>
-    <div class="row">
-    <div class="col-12 mb-1">
-        <button type="button" class="btn btn-submit btn-info btn-block"  accesskey="_form" confirm="この予定を確認済みに更新しますか？">
-          <i class="fa fa-envelope mr-1"></i>
-          送信
-        </button>
-      </form>
-    </div>
+    @if($user->role=="parent" || $user->role=="student")
+      <div class="row">
+        @component('calendars.forms.fix_form', ['item' => $item, 'user'=>$user]) @endcomponent
+        @component('calendars.forms.target_member', ['item' => $item, 'user'=>$user, 'status'=>'fix', 'student_id' => $student_id]) @endcomponent
+      </div>
+      <div class="row">
+      <div class="col-12 mb-1">
+          <button type="button" class="btn btn-submit btn-info btn-block"  accesskey="_form" confirm="この予定を確認済みに更新しますか？">
+            <i class="fa fa-envelope mr-1"></i>
+            送信
+          </button>
+        </form>
+      </div>
+    @endif
     <div class="col-12 col-lg-12 col-md-12 mb-1">
         <button type="reset" class="btn btn-secondary btn-block">
             閉じる
