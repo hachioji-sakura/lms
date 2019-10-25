39d3fa00721b8ebd3491cae04efdd0c586bd43ba
hideo-yasui <yasumihosi@gmail.com>
2019-10-25 17:19:02 +0900

    ﾊﾞｸﾞ修正

--- resources/lang/ja/labels.php
+++ resources/lang/ja/labels.php
@@ -63,6 +63,7 @@ return [
   'month_schedule_list' => '今月授業予定',
   'adjust_schedule_list' => '予定調整中',
   'rest_contact' => '休み連絡',
+  'rest_schedule_list' => '休み予定',
   'exchange_schedule_list' => '振替対象',
   'schedule_history' => '授業履歴',
   'trials_schedule_history' => '体験授業履歴',
