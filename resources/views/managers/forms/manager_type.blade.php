0da099e053ee2c58826db7815a13b00861416e51
hideo-yasui <yasumihosi@gmail.com>
2019-10-31 11:14:16 +0900

    事務権限をradioに変更 （タグの意味合いだと複数つけるべき）

--- resources/views/managers/forms/manager_type.blade.php
+++ resources/views/managers/forms/manager_type.blade.php
@@ -5,7 +5,7 @@
       <span class="right badge badge-secondary ml-1">{{__('labels.optional')}}</span>
     </label>
     <label class="mx-2">
-      <input type="checkbox" value="disabled" name="manager_type[]" class="icheck flat-grey"
+      <input type="radio" value="disabled" name="manager_type[]" class="icheck flat-grey"
         onChange="tag_change(this);"
       @if(isset($item) && $item->user->has_tag('manager_type', 'disabled')===true)
       checked
@@ -14,7 +14,7 @@
     </label>
     @foreach($attributes['manager_type'] as $index => $name)
     <label class="mx-2">
-      <input type="checkbox" value="{{ $index }}" name="manager_type[]" class="icheck flat-green"
+      <input type="radio" value="{{ $index }}" name="manager_type[]" class="icheck flat-green"
       @if(isset($item) && $item->user->has_tag('manager_type', $index)===true)
       checked
       @endif
