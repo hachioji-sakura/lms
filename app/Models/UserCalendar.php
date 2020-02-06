4770a25b8f0c6dfcbbb35cc9af072adb02f5cfe2
hideo-yasui <yasumihosi@gmail.com>
2020-02-06 13:55:40 +0900

    work=11の対応

--- app/Models/UserCalendar.php
+++ app/Models/UserCalendar.php
@@ -412,6 +412,7 @@ EOT;
       case 4:
         return 'examination_director';
       case 5:
+      case 11:
         return 'training';
       case 6:
       case 7:
@@ -431,7 +432,7 @@ EOT;
   public function get_teaching_type($work=''){
     if(empty($work)) $work = $this->work;
     if($work==10) return 'season';
-    if($work==11) return 'training';
+    if($work==11) return 'season_training';
     if($work==5) return 'training';
 
     $ret = "";
