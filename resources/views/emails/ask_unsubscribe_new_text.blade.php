7d1fe7f04adf82563918a28b7933431d5d31ad86
hideo-yasui <yasumihosi@gmail.com>
2020-04-10 15:49:03 +0900

    休会・退会登録メール

---
+++ resources/views/emails/ask_unsubscribe_new_text.blade.php
@@ -0,0 +1,12 @@
+@include('emails.common')
+
+以下の退会依頼を登録しました。
+
+…………………………………………………………………………………………
+{{__('labels.'.$ask->target_model)}}{{__('labels.name')}}: {{$target_model->name()}}
+{{__('labels.unsubscribe')}}{{__('labels.date')}}: {{$ask["label_start_date"]}}
+{{__('labels.unsubscribe')}}{{__('labels.reason')}}:
+{{$ask->body}}
+…………………………………………………………………………………………
+
+@yield('signature')
