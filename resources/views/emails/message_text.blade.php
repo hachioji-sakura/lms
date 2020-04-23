6e5e56213dac6013240e14c206cb5937e72e9376
osuke-kakuta <osuke.kakuta@gmail.com>
2020-04-23 15:08:18 +0900

    メール文面変更

--- resources/views/emails/message_text.blade.php
+++ resources/views/emails/message_text.blade.php
@@ -2,20 +2,10 @@
 
 {{$item->target_user->details()->name}} さん
 
-{{$item->create_user->details()->name}} さん
-からメッセージが届きました。
+{{$item->create_user->details()->name}} さん からメッセージが届きました。
 
-概要:{{$item->title}}
+{{$item->title}}
 
-内容:
 {{$item->body}}
 
-
-【{{__('labels.important')}}】
-{{__('messages.mail_footer')}}
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-{{__('labels.email')}}：hachiojisakura-support@hachioji-sakura.com
-{{__('labels.phone_no')}}　：080-7726-2443 (042-649-3976)
-{{config('app.management_url')}}
-━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-Copyright © 国立・八王子・日野の個別指導塾 さくら One All Rights Reserved.
+※返信はシステムにログインして行ってください。
