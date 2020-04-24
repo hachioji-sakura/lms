1befcc942a3c6e6e8bcb864418818066b92e2d60
osuke-kakuta <osuke.kakuta@gmail.com>
2020-04-24 14:39:16 +0900

    studentのid長さ制限追加

--- resources/views/students/forms/student_id.blade.php
+++ resources/views/students/forms/student_id.blade.php
@@ -11,12 +11,12 @@
         <span>{{$item->email}}</span>
         <a href="javascript:void(0);" onClick="email_form_edit()" class="btn btn-sm btn-success ml-2"><i class="fa fa-edit"></i></a>
       </div>
-      <div class="w-100 email-edit" style="display:none;">
-        <input type="text" id="student_id" name="email" class="form-control w-50 float-left" placeholder="任意の英数字"  required="true" inputtype="alnum" query_check="users/email" query_check_error="このログインIDはすでに使われています。別のIDを入力してください。" value="{{$item['email']}}">
+      <div id="email-edit" class="w-100 email-edit" style="display:none;">
+        <input type="text" id="student_id" name="email" class="form-control w-50 float-left" placeholder="任意の英数字"  required="true" inputtype="alnum"  minlength=3 maxlength=32 query_check="users/email" query_check_error="このログインIDはすでに使われています。別のIDを入力してください。" value="{{$item['email']}}">
         <a href="javascript:void(0);" onClick="email_form_edited()" class="btn btn-sm btn-success float-left mt-1 ml-2 mb-2"><i class="fa fa-check"></i></a>
       </div>
     @else
-      <input type="text" id="student_id" name="email" class="form-control" placeholder="任意の英数字" required="true" inputtype="alnum" query_check="users/email" query_check_error="このログインIDはすでに使われています。別のIDを入力してください。">
+      <input type="text" id="student_id" name="email" class="form-control" placeholder="任意の英数字" required="true" inputtype="alnum" query_check="users/email"  minlength=3 maxlength=32 query_check_error="このログインIDはすでに使われています。別のIDを入力してください。">
     @endif
   </div>
 </div>
@@ -30,16 +30,14 @@ function email_form_edited(){
   var new_email = $('input[name="email"]').val();
   var is_check = false;
   console.log(email+'=='+new_email);
-  if(email == new_email){
-    is_check = true;
-  }
-  else if(front.validateFormValue('email_form')){
+  if(front.validateFormValue('email-edit')){
     is_check = true;
   }
   if(is_check){
     $('div.email-edit').hide();
     $('div.email-edited').show();
     $('div.email-edited span').html(new_email);
+    $('div.error_message').hide();
   }
   return is_check;
 }
