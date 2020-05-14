6127235faf0d72684e246d2fca3ea28528db22c5
osuke-kakuta <osuke.kakuta@gmail.com>
2020-05-14 11:44:58 +0900

    コミット忘れ

---
+++ resources/views/tasks/confirm.blade.php
@@ -0,0 +1,19 @@
+<div id="{{$action}}_form">
+  <div class="row">
+    <div class="col-6">
+      <form method="POST" action="/tasks/{{$item->id}}/{{$action}}">
+        @csrf
+        @method('PUT')
+        <button type="submit" class="btn btn-submit btn-primary w-100">
+          OK
+        </button>
+      </form>
+    </div>
+    <div class="col-6">
+      <a href="javascript:void(0);" data-dismiss="modal" role="button" class="btn btn-secondary btn-block float-left mr-1">
+        <i class="fa fa-times-circle mr-1"></i>
+        {{__('labels.back_button')}}
+      </a>
+    </div>
+  </div>
+</div>
