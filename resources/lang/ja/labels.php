f5575d1e64fc226139a4c4ffca0d1b0fef967525
hideo-yasui <yasumihosi@gmail.com>
2020-05-14 13:02:19 +0900

    設定開始日　＝　入力必須にし、ラベルは授業開始日 設定終了日　＝　入力任意にし、ラベルは授業終了日

--- resources/lang/ja/labels.php
+++ resources/lang/ja/labels.php
@@ -331,6 +331,7 @@ return [
   'not_require_student_confirm' => '生徒の確認を必要としない',
   'schedule_start_hope_date' => '授業開始希望日',
   'schedule_start_date' => '授業開始日',
+  'schedule_end_date' => '授業終了日',
   'verification_code' => '認証コード',  'sheat' => '座席',
   'result' => '結果',
   'reset' => 'リセット',
