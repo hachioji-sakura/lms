76112a708fae76963434310653ead8af990dd111
hideo-yasui <yasumihosi@gmail.com>
2020-07-02 14:38:29 +0900

    振替の場合、追加メッセージの注意を表示しない

--- resources/views/calendars/create_form.blade.php
+++ resources/views/calendars/create_form.blade.php
@@ -1,10 +1,12 @@
 @section('first_form')
 <div class="row">
   @if($item->work!=9)
-    @if($item->trial_id == 0)
-    <div class="alert alert-warning text-sm pr-2 schedule_type schedule_type_class">
-      <h5><i class="icon fa fa-exclamation-triangle"></i> {{__('labels.important')}}</h5>
-      {!!nl2br(__('messages.warning_schedule_add'))!!}
+    @if($item->trial_id == 0 && $item["exchanged_calendar_id"]==0)
+    <div class="col-12">
+      <div class="alert alert-warning text-sm pr-2 schedule_type schedule_type_class">
+        <h5><i class="icon fa fa-exclamation-triangle"></i> {{__('labels.important')}}</h5>
+        {!!nl2br(__('messages.warning_schedule_add'))!!}
+      </div>
     </div>
     @endif
     @component('calendars.forms.select_teacher', ['_edit'=>$_edit, 'teachers'=>$teachers]); @endcomponent
