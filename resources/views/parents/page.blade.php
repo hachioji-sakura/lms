fb706157c5b42c0f598df808fc5ee3dbbccec62c
hideo-yasui <yasumihosi@gmail.com>
2020-07-11 15:52:12 +0900

    生徒登録はまだ出せない

--- resources/views/parents/page.blade.php
+++ resources/views/parents/page.blade.php
@@ -54,6 +54,7 @@
               登録生徒
             </h3>
             <div class="card-title text-sm">
+              {{--
               <ul class="pagination pagination-sm m-0 float-left text-sm">
                 <li class="page-item">
                   <a class="btn btn-info btn-sm" href="javascript:void(0);" page_form="dialog" page_url="/students/create?student_parent_id={{$item->id}}" page_title="生徒追加">
@@ -62,6 +63,7 @@
                   </a>
                 </li>
               </ul>
+              --}}
             </div>
           </div>
           <!-- /.card-header -->
