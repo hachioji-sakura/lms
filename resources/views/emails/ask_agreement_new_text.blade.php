1adf0d3d8ea0bc391cd7197a9fd8da9f1b41810b
hideo-yasui <yasumihosi@gmail.com>
2020-08-27 15:09:29 +0900

    契約メール文面調整

--- resources/views/emails/ask_agreement_new_text.blade.php
+++ resources/views/emails/ask_agreement_new_text.blade.php
@@ -11,8 +11,7 @@
 ご契約内容
 ■基本契約内容
 ・レッスン : {{$target_model->student->tags_name('lesson')}}
-{{-- TODO 修正lesson_week_count --}}
-・通塾回数/週 : 週{{$target_model->student->user->get_enable_calendar_setting_count()}}回
+・通塾回数/週 : 週{{$target_model->student->user->get_enable_calendar_setting_count()}}回 {{-- TODO 修正lesson_week_count --}}
 ・授業時間 : {{$target_model->student->tag_name('course_minutes')}}
 @if($target_model->student->user->has_tag('lesson',2)==true)
 ・英会話コース : {{$target_model->student->tag_name('english_talk_course_type')}}
@@ -22,6 +21,7 @@
 @endif
 ・入会金 : @if($target_model->student->is_first_brother()==true) @component('trials.forms.entry_fee', ['user'=>$target_model->student->user]) @endcomponent @else 0円 @endif
 ・月会費 : @component('trials.forms.monthly_fee', ['user'=>$target_model->student->user, 'is_text' => true]) @endcomponent
+
 ■通塾内容
 <?php
 $tuition_form = [];
@@ -33,6 +33,7 @@ $is_exist = false;
 <?php
 $setting = $setting->details();
 ?>
+-----------------------------
 ・概要：{{$setting->lesson()}} / {{$setting->course()}}
 ・{{$setting->schedule_method()}}{{$setting["week_setting"]}}/{{$setting["timezone"]}}
 ・授業時間：{{$setting["course_minutes_name"]}}
@@ -55,6 +56,7 @@ $setting_key .= $setting->get_tag_value('kids_lesson');
 }
 ?>
 ・受講料：@if(!empty($target_model->student->get_tuition($setting, false))) &yen;{{$target_model->student->get_tuition($setting, false)}} / 時間 @else 受講料設定がありません @endif
+-----------------------------
 
 		@endforeach
 	@endforeach
