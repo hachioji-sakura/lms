c4fa304711d1da1cb4652130062c3a08c89e24db
hideo-yasui <yasumihosi@gmail.com>
2020-08-29 05:56:58 +0900

    入会案内の受講料を変更しても、変わらないバグを対応

--- app/Models/UserCalendarMemberSetting.php
+++ app/Models/UserCalendarMemberSetting.php
@@ -378,13 +378,12 @@ class UserCalendarMemberSetting extends UserCalendarMember
     return $res;
   }
   public function set_api_lesson_fee($lesson_fee=null){
-    if($this->setting->is_teaching()!=true){
 
+    if($this->setting->is_teaching()!=true){
       return null;
     }
     $user = $this->user->details();
     if($user->role!='student'){
-      \Log::warning("d1-2 user_role=".$user->role);
       return null;
     }
 
@@ -428,7 +427,7 @@ class UserCalendarMemberSetting extends UserCalendarMember
     $tuition = $this->get_tuition();
 
     if($tuition == null){
-      \Log::warning("d3");
+
       Tuition::add([
         'student_id' => $user->id,
         'teacher_id' => $teacher->id,
@@ -447,11 +446,14 @@ class UserCalendarMemberSetting extends UserCalendarMember
       ]);
     }
     else {
+
       $tuition->update([
         'title' => $setting_details['title'],
         'tuition' => $lesson_fee,
       ]);
     }
+
+
     return $this->api_response(200, '', '');
   }
   //TODO : status_updateをcalendar_setting向けに最適帰化
