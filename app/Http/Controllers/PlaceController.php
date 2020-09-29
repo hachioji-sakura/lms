4ead69287e3cedf89f2d51f84b6516ab735cae66
hideo-yasui <yasumihosi@gmail.com>
2020-09-29 19:23:59 +0900

    TeacherController / StudentController 共通化
    UserController
    filter:検索パラメータ追加
    
    Place / PlaceFloor / PlaceSheat
    Crud追加

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -3,82 +3,212 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Place;
 
-class PlaceController extends Controller
+class PlaceController extends MilestoneController
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+  public $domain = "places";
+  public function model(){
+    return Place::query();
+  }
+  public function show_fields($type){
+    $fields = [
+      "name" => [
+        "label" => "名称",
+        'size' => 4
+      ],
+      "name_en" => [
+        "label" => "英語名",
+        'size' => 4
+      ],
+      "sort_no" => [
+        "label" => "表示順",
+        'size' => 2
+      ],
+      "is_use" => [
+        "label" => "データ利用",
+        'size' => 2
+      ],
+      "post_no" => [
+        "label" => "郵便番号",
+        'size' => 4
+      ],
+      "address" => [
+        "label" => '所在地',
+        'size' => 8
+      ],
+      "phone_no" => [
+        "label" => '連絡先',
+      ],
+    ];
+    $fields['created_date'] = [
+      'label' => __('labels.add_datetime'),
+      'size' => 6
+    ];
+    $fields['updated_date'] = [
+      'label' => __('labels.upd_datetime'),
+      'size' => 6
+    ];
+    return $fields;
+  }
+  public function list_fields(){
+    $fields = [
+      'id' => [
+        'label' => 'ID',
+        "link" => "show",
+      ],
+      "name" => [
+        "label" => "名称",
+        "link" =>  function($row){
+          return "/place_floors?place_id=".$row->id;
+        },
+      ],
+      "name_en" => [
+        "label" => "英語名",
+      ],
+      "sort_no" => [
+        "label" => "表示順",
+      ],
+      "post_no" => [
+        "label" => "郵便番号",
+      ],
+      "address" => [
+        "label" => '所在地',
+      ],
+      "phone_no" => [
+        "label" => '連絡先',
+      ],
+      "updated_date" => [
+        "label" => __('labels.upd_datetime'),
+      ],
+      "buttons" => [
+        "label" => __('labels.control'),
+        "button" => [
+          "edit",
+          "delete"]
+        ],
+    ];
+    return $fields;
+  }
+
+  public function create_form(Request $request){
+    $user = $this->login_details($request);
+    $form = [];
+    $form['address'] = $request->get('address');
+    $form['phone_no'] = $request->get('phone_no');
+    $form['post_no'] = $request->get('post_no');
+    $form['sort_no'] = $request->get('sort_no');
+    $form['name'] = $request->get('name');
+    $form['name_en'] = $request->get('name_en');
+    return $form;
+  }
+  /**
+   * データ更新時のパラメータチェック
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function save_validate(Request $request)
+  {
+    $form = $request->all();
+    //保存時にパラメータをチェック
+    if(empty($form['name']) || empty($form['name_en'])){
+      return $this->bad_request('リクエストエラー', 'name='.$form['name'].'/name_en='.$form['name_en']);
+    }
+    return $this->api_response(200, '', '');
+  }
+  public function update_form(Request $request){
+    $form = [];
+    $form['address'] = $request->get('address');
+    $form['phone_no'] = $request->get('phone_no');
+    $form['post_no'] = $request->get('post_no');
+    $form['sort_no'] = $request->get('sort_no');
+    $form['name'] = $request->get('name');
+    $form['name_en'] = $request->get('name_en');
+    return $form;
+  }
+  public function get_param(Request $request, $id=null){
+    $ret = parent::get_param($request, $id);
+    if($this->is_manager($ret['user']->role)!==true){
+      abort(403);
+    }
+    if($request->has('place_id')){
+      $ret['place_id'] = $request->get('place_id');
+    }
+    return $ret;
+  }
+  /**
+   * 検索～一覧
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return [Collection, field]
+   */
+  public function search(Request $request)
+  {
+    $param = $this->get_param($request);
+    $request->merge([
+      '_sort_order' => 'asc',
+      '_sort' => 'sort_no',
+    ]);
+    if($request->has('is_desc') && $request->get('is_desc')==1){
+      $request->merge([
+        '_sort_order' => 'asc',
+      ]);
+    }
+    $items = $this->model();
+    $user = $this->login_details($request);
+    $items = $this->_search_scope($request, $items);
+    $items = $this->_search_sort($request, $items)->paginate($param['_line']);
+
+    return ['items' => $items, 'fields' => $this->list_fields()];
+  }
+  /**
+   * フィルタリングロジック
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  Collection $items
+   * @return Collection
+   */
+  public function _search_scope(Request $request, $items)
+  {
+    //ID 検索
+    if(isset($request->id)){
+      $items = $items->where('id',$request->id);
+    }
+    if(isset($request->post_no)){
+      $items = $items->where('post_no', 'like', $request->post_no.'%');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    //検索ワード
+    if(isset($request->search_word)){
+      $items = $items->searchWord($request->search_word);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    return $items;
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+   public function _update(Request $request, $id)
+   {
+     $res = $this->save_validate($request);
+     if(!$this->is_success_response($res)){
+       return $res;
+     }
+     $res =  $this->transaction($request, function() use ($request, $id){
+       $form = $this->update_form($request);
+       $item = $this->model()->where('id', $id)->first();
+       $item->update($form);
+       return $this->api_response(200, '', '', $item);
+     }, '更新しました。', __FILE__, __FUNCTION__, __LINE__ );
+     return $res;
+   }
+   public function _delete(Request $request, $id)
+   {
+     $form = $request->all();
+       $item = $this->model()->where('id', $id)->first();
+       $ret = $item->dispose();
+       if(!$this->is_success_response($ret)) return $ret;
+       return $this->api_response(200, '', '', $item);
+     $res = $this->transaction($request, function() use ($request, $form, $id){
+     }, '削除しました。', __FILE__, __FUNCTION__, __LINE__ );
+     return $res;
+   }
 }
--- app/Models/PlaceFloor.php
+++ app/Models/PlaceFloor.php
@@ -4,58 +4,85 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\UserCalendar;
+use App\Models\Traits\Common;
 
 class PlaceFloor extends Model
 {
+  use common;
   protected $connection = 'mysql_common';
   protected $table = 'common.place_floors';
   protected $guarded = array('id');
 
   public static $rules = array(
-      'name' => 'required',
+    'name' => 'required',
   );
+  /**
+   *　リレーション：所在地
+   */
+  public function place(){
+    return $this->belongsTo('App\Models\Place', 'place_id');
+  }
+  /**
+   *　リレーション：座席
+   */
+  public function sheats(){
+    return $this->hasMany('App\Models\PlaceFloorSheat', 'place_floor_id');
+  }
+  public function getSheatCountAttribute(){
+    if(isset($this->sheats)) return count($this->sheats);
+    return 0;
+  }
+  public function name(){
+    if(app()->getLocale()=='en') return $this->name_en;
+    return $this->name;
+  }
+  public function is_use(){
+    $c = UserCalendar::where('place_floor_id', $this->id)->count();
 
-    /**
-     *　リレーション：所在地
-     */
-    public function place(){
-      return $this->belongsTo('App\Models\Place', 'place_id');
+    if($c>0) return true;
+    foreach($this->sheats as $sheat){
+      if($sheat->is_use()==true) return true;
     }
-    /**
-     *　リレーション：座席
-     */
-    public function sheats(){
-      return $this->hasMany('App\Models\PlaceFloorSheat', 'place_floor_id');
+    return false;
+  }
+  public function getIsUseAttribute(){
+    if($this->is_use()==true) return '使用中';
+    return '未使用';
+  }
+  public function dispose(){
+    if($this->is_use()==true){
+      return $this->error_response('このデータはカレンダーにて使用されており削除できません');
     }
-    public function name(){
-      if(app()->getLocale()=='en') return $this->name_en;
-      return $this->name;
-    }
-    public function get_free_seat($start_time, $end_time){
-      //指定した時間帯・フロアのカレンダーを取得(新規、キャンセル、休み、休講は利用扱いではない）
-      $calendars = UserCalendar::searchDate($start_time, $end_time)
-                ->findStatuses(['cancel', 'rest', 'lecture_cancel', 'new'], true)
-                ->where('place_floor_id', $this->id)->get();
-      foreach($this->sheats as $sheat){
-        $is_use = false;
-        foreach($calendars as $calendar){
-          foreach($calendar->members as $member){
-            if($member->is_active()==true && $member->place_floor_sheat_id == $sheat->id){
-              //この座席は利用予定（予約済み）
-              $is_use = true;
-              break;
-            }
-          }
-          if($is_use===true){
+
+    PlaceFloorSheat::where('place_floor_id', $this->id)->delete();
+    $this->delete();
+  }
+  public function get_free_seat($start_time, $end_time){
+    //指定した時間帯・フロアのカレンダーを取得(新規、キャンセル、休み、休講は利用扱いではない）
+    $calendars = UserCalendar::searchDate($start_time, $end_time)
+              ->findStatuses(['cancel', 'rest', 'lecture_cancel', 'new'], true)
+              ->where('place_floor_id', $this->id)->get();
+    foreach($this->sheats as $sheat){
+      $is_use = false;
+      foreach($calendars as $calendar){
+        foreach($calendar->members as $member){
+          if($member->is_active()==true && $member->place_floor_sheat_id == $sheat->id){
+            //この座席は利用予定（予約済み）
+            $is_use = true;
             break;
           }
         }
-        if($is_use===false){
-          //使われていない座席を返す
-          return $sheat;
+        if($is_use===true){
+          break;
         }
       }
-      //空き座席なし
-      return null;
+      if($is_use===false){
+        //使われていない座席を返す
+        return $sheat;
+      }
     }
+    //空き座席なし
+    return null;
+  }
+
 }
