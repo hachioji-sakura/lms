309ec49a572a077a9a1543da0824c2e721ac463c
hideo-yasui <yasumihosi@gmail.com>
2020-11-21 12:07:18 +0900

    URLの文字数拡張

---
+++ database/migrations/2020_11_21_114603_change_url_action_logs.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUrlActionLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('mysql_common')->table('action_logs', function (Blueprint $table) {
+          $table->string('url', 10000)->comment('REQUEST_URI')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('action_logs', function (Blueprint $table) {
+          $table->string('url')->comment('REQUEST_URI')->change();
+        });
+    }
+}
