17bd53838e2d05e22e242dca573a991527a70c85
hideo-yasui <yasumihosi@gmail.com>
2021-03-06 11:21:09 +0900

    strtotimeが9999-12-31を拾えないときがある

--- app/Models/Traits/Common.php
+++ app/Models/Traits/Common.php
@@ -273,6 +273,7 @@ trait Common
   public function is_publiced(){
     if(!isset($this->publiced_at)) return false;
     if(empty($this->publiced_at)) return false;
+    if($this->publiced_at=='9999-12-31') return false;
     if(strtotime($this->publiced_at) <= strtotime('now')) return true;
     return false;
   }
