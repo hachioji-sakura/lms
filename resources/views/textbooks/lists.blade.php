911cfa0686255fcb4a3c3a42152beb247da8cc0e
kasaibksn <kasaibksn@gmail.com>
2021-03-30 16:05:53 +0900

    #802 @include('dashboard.lists')を使用するよう修正

--- resources/views/textbooks/lists.blade.php
+++ resources/views/textbooks/lists.blade.php
@@ -17,58 +17,6 @@
       <i class="fa fa-plus nav-icon"></i>{{$domain_name}} {{__('labels.add')}}
     </a>
   </li>
-{{--    <li class="nav-item has-treeview menu-open mt-2">--}}
-{{--      <a href="#" class="nav-link">--}}
-{{--        <i class="nav-icon fa fa-filter"></i>--}}
-{{--        <p>--}}
-{{--          {{__('labels.filter')}}--}}
-{{--          <i class="right fa fa-angle-left"></i>--}}
-{{--        </p>--}}
-{{--      </a>--}}
-{{--      <ul class="nav nav-treeview">--}}
-{{--        <li class="nav-item">--}}
-{{--          <a href="/{{$domain}}" class="nav-link">--}}
-{{--            <i class="fa fa-calendar nav-icon"></i>すべて--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--        <li class="nav-item">--}}
-{{--          <a href="/{{$domain}}?search_status[]=new&search_status[]=confirm" class="nav-link--}}
-{{--        @if(isset($filter['calendar_filter']['search_status']) && in_array('new', $filter['calendar_filter']['search_status'])==true && in_array('confirm', $filter['calendar_filter']['search_status'])==true)--}}
-{{--            active--}}
-{{--        @endif--}}
-{{--            ">--}}
-{{--            <i class="fa fa-exclamation-triangle nav-icon"></i>未確定--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--        <li class="nav-item">--}}
-{{--          <a href="/{{$domain}}?search_status[]=fix" class="nav-link--}}
-{{--         @if(isset($filter['calendar_filter']['search_status']) && in_array('fix', $filter['calendar_filter']['search_status'])==true)--}}
-{{--            active--}}
-{{--@endif--}}
-{{--            ">--}}
-{{--            <i class="fa fa-check-circle nav-icon"></i>確定--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--        <li class="nav-item">--}}
-{{--          <a href="/{{$domain}}?search_status[]=absence&search_status[]=rest&search_status[]=lecture_cancel" class="nav-link--}}
-{{--       @if(isset($filter['calendar_filter']['search_status']) && in_array('absence', $filter['calendar_filter']['search_status'])==true && in_array('rest', $filter['calendar_filter']['search_status'])==true && in_array('lecture_cancel', $filter['calendar_filter']['search_status'])==true)--}}
-{{--            active--}}
-{{--@endif--}}
-{{--            ">--}}
-{{--            <i class="fa fa-calendar-times nav-icon"></i>休み--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--        <li class="nav-item">--}}
-{{--          <a href="/{{$domain}}?search_status[]=cancel" class="nav-link--}}
-{{--      @if(isset($filter['calendar_filter']['search_status']) && in_array('cancel', $filter['calendar_filter']['search_status'])==true)--}}
-{{--            active--}}
-{{--@endif--}}
-{{--            ">--}}
-{{--            <i class="fa fa-ban nav-icon"></i>キャンセル--}}
-{{--          </a>--}}
-{{--        </li>--}}
-{{--      </ul>--}}
-{{--    </li>--}}
 </ul>
 @endsection
 
@@ -86,24 +34,5 @@
 </div>
 @endsection
 
-@section('contents')
-<div class="card">
-  <div class="card-header">
-    <h3 class="card-title">@yield('title')</h3>
-    <div class="card-title text-sm">
-      @yield('list_pager')
-    </div>
-    <div class="card-tools">
-      @component('components.search_word', ['search_word' => $search_word])
-      @endcomponent
-    </div>
-  </div>
-  <div class="card-body table-responsive p-0">
-    @component('components.list', ['items' => $items, 'fields' => $fields, 'domain' => $domain, 'domain_name' => $domain_name])
-    @endcomponent
-  </div>
-</div>
-@yield('list_filter')
-@endsection
-
+@include('dashboard.lists')
 @extends('dashboard.common')
