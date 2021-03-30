567b8a8835463e715ac2c71050b6478faa247046
engineer-work-take <fullstackengineer.work@gmail.com>
2021-03-31 01:33:51 +0900

    [fix] win対応版への修正

--- app/Supports/CSVReader.php
+++ app/Supports/CSVReader.php
@@ -14,30 +14,6 @@ class CSVReader
     /**
      * URL指定による外部サイトからのCSV読み取り
      *
-     * @param string $url
-     * @return array
-     */
-    public function readByUrl(string $url): array
-    {
-        $results = file_get_contents($url);
-        $results = mb_convert_encoding($results, 'utf-8', 'UTF-8, sjis-win');
-        $temp = tmpfile();
-        $csv = [];
-        
-        fwrite($temp, $results);
-        rewind($temp);
-        
-        while ($results = fgetcsv($temp)) {
-            $csv[] = $results;
-        }
-        fclose($temp);
-        
-        return $csv;
-    }
-    
-    /**
-     * URL指定による外部サイトからのCSV読み取り
-     *
      * 1行目を配列のキーとして情報を取得することができます
      *
      * @param string $url
@@ -67,5 +43,41 @@ class CSVReader
         }
         
         return $results;
+    }
+    
+    /**
+     * URL指定による外部サイトからのCSV読み取り
+     *
+     * @param string $url
+     * @return array
+     */
+    public function readByUrl($url)
+    {
+        $str = file_get_contents($url);
+        $is_win = strpos(PHP_OS, 'WIN') === 0;
+        
+        // Windowsの場合は Shift_JIS、Unix系は UTF-8で処理
+        if ($is_win) {
+            setlocale(LC_ALL, 'Japanese_Japan.932');
+        } else {
+            setlocale(LC_ALL, 'ja_JP.UTF-8');
+            $str = mb_convert_encoding($str, 'utf-8', 'UTF-8, sjis-win');
+        }
+        
+        $result = [];
+        $fp = fopen('php://temp', 'r+');
+        fwrite($fp, str_replace(["\r\n", "\r"], "\n", $str));
+        rewind($fp);
+        while ($row = fgetcsv($fp)) {
+            // windows の場合はSJIS-win → UTF-8 変換
+            $result[] = $is_win
+                ? array_map(function ($val) {
+                    return mb_convert_encoding($val, 'UTF-8', 'SJIS-win');
+                }, $row)
+                : $row;
+        }
+        fclose($fp);
+        
+        return $result;
     }
 }
