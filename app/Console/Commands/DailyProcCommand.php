66f3419dfe4a2781ed63ad622f3ed358edb8ad2d
hideo-yasui <yasumihosi@gmail.com>
2021-04-09 11:48:22 +0900

    実行日時の次の日以前の体験授業予定にて、 確定していない予定をキャンセルする

--- app/Console/Commands/DailyProcCommand.php
+++ app/Console/Commands/DailyProcCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Ask;
+use App\Models\UserCalendar;
 use App\Http\Controllers\Controller;
 
 class DailyProcCommand extends Command
@@ -44,93 +45,74 @@ class DailyProcCommand extends Command
     }
     public function daily_proc($d='')
     {
-      if(empty($d)) $d = date('Y-m-d');
-      $result = ['asks'=>[], 'recess'=>[],'unsubscribe'=>[]];
-      //退会・休会申請にて承認済みの依頼を取得
-      //開始＝本日（指定日）
-      $this->info("daily_proc[d=".$d."]");
-      @$this->send_slack("daily_proc[d=".$d."]", 'warning', "daily_proc");
-      $asks = Ask::where('status', 'commit')->findTypes(['recess', 'unsubscribe'])
-        ->get();
-      foreach($asks as $ask){
-        //対象のモデルを取得
-        $target_model_data = $ask->get_target_model_data();
-        if($target_model_data==null) continue;
-        $result['asks'][] = $ask;
-        $this->info("休会,退会　ask proc[id=".$ask->id."]");
-        if($ask->type=="recess"){
-          //休会（休職）
-          $ret = $target_model_data->recess();
-        }
-        else if($ask->type=="unsubscribe"){
-          //退会（退職）
-          $ret = $target_model_data->unsubscribe();
-        }
+      $this->ask_auto_proc($d);
+      $this->calendar_auto_cancel($d);
+    }
+    public function ask_auto_proc($d){
+     if(empty($d)) $d = date('Y-m-d');
+     $result = ['asks'=>[], 'recess'=>[],'unsubscribe'=>[]];
+     //退会・休会申請にて承認済みの依頼を取得
+     //開始＝本日（指定日）
+     $this->info("daily_proc[d=".$d."]");
+     @$this->send_slack("daily_proc[d=".$d."]", 'warning', "daily_proc");
+     $asks = Ask::where('status', 'commit')->findTypes(['recess', 'unsubscribe'])
+       ->get();
+     foreach($asks as $ask){
+       //対象のモデルを取得
+       $target_model_data = $ask->get_target_model_data();
+       if($target_model_data==null) continue;
+       $result['asks'][] = $ask;
+       $this->info("休会,退会　ask proc[id=".$ask->id."]");
+       if($ask->type=="recess"){
+         //休会（休職）
+         $ret = $target_model_data->recess();
+       }
+       else if($ask->type=="unsubscribe"){
+         //退会（退職）
+         $ret = $target_model_data->unsubscribe();
+       }
 
-        if($ret!=null){
-          //成功した場合
-          if(isset($ret['user_calendar_members'])){
-            $target_model_data['user_calendar_members'] = $ret['user_calendar_members'];
-          }
-          $result[$ask->type][] = $target_model_data;
-          if(($ask->type=="unsubscribe" && $target_model_data->status=='unsubscribe') ||
-              ($ask->type=="recess" && $target_model_data->status=='regular')){
-            //退会の場合は、承認済み→完了
-            $ask->complete();
-            $this->info("success[id=".$ask->id."]");
-            @$this->send_slack("success[id=".$ask->id."][type=".$ask->type."]", 'warning', "daily_proc");
-          }
-        }
-        else {
-          //失敗
-          $this->info("failed[id=".$ask->id."][".$target_model_data->status."]");
-          @$this->send_slack("failed[id=".$ask->id."][type=".$ask->type."]", 'warning', "daily_proc");
-        }
-      }
-      //休会キャンセルの処理
-      //$d = date("Y-m-d",strtotime("-1 day ".$d));
-      /*
-      $asks = Ask::where('status', 'cancel')->findTypes(['recess', 'unsubscribe'])
-        ->get();
-      foreach($asks as $ask){
-        //対象のモデルを取得
-        $target_model_data = $ask->get_target_model_data();
-        if($target_model_data==null) continue;
-        $result['asks'][] = $ask;
-
-        $this->info("ask cancel proc[id=".$ask->id."]");
-        if($ask->type=="recess"){
-          //休会（休職）
-          $ret = $target_model_data->recess_cancel();
-        }
-        else if($ask->type=="unsubscribe"){
-          //退会（退職）
-          $ret = $target_model_data->unsubscribe_cancel();
-        }
-
-        $ret = $target_model_data->recess_cancel();
-        if($ret!=null){
-          if(isset($ret['user_calendar_members'])){
-            //有効化されたカレンダー
-            $target_model_data['user_calendar_members'] = $ret['user_calendar_members'];
-          }
-          if(isset($ret['conflict_calendar_members'])){
-            //有効化したら競合したカレンダー
-            $target_model_data['conflict_calendar_members'] = $ret['conflict_calendar_members'];
-          }
-          $result[$ask->type][] = $target_model_data;
-          $ask->complete();
-          $this->info("success[id=".$ask->id."]");
-          @$this->send_slack("success[id=".$ask->id."][type=".$ask->type."]", 'warning', "daily_proc");
-        }
-        else {
-          //失敗
-          $this->info("failed[id=".$ask->id."]");
-          @$this->send_slack("failed[id=".$ask->id."][type=".$ask->type."]", 'warning', "daily_proc");
-        }
-      }
-      */
-      return $result;
+       if($ret!=null){
+         //成功した場合
+         if(isset($ret['user_calendar_members'])){
+           $target_model_data['user_calendar_members'] = $ret['user_calendar_members'];
+         }
+         $result[$ask->type][] = $target_model_data;
+         if(($ask->type=="unsubscribe" && $target_model_data->status=='unsubscribe') ||
+             ($ask->type=="recess" && $target_model_data->status=='regular')){
+           //退会の場合は、承認済み→完了
+           $ask->complete();
+           $this->info("success[id=".$ask->id."]");
+           @$this->send_slack("success[id=".$ask->id."][type=".$ask->type."]", 'warning', "daily_proc");
+         }
+       }
+       else {
+         //失敗
+         $this->info("failed[id=".$ask->id."][".$target_model_data->status."]");
+         @$this->send_slack("failed[id=".$ask->id."][type=".$ask->type."]", 'warning', "daily_proc");
+       }
+     }
+     return $result;
+   }
+   public function calendar_auto_cancel($d){
+     //実行時の予定の前日に、体験授業で確認待ちで残っている予定
+     if(empty($d)) $d = date('Y-m-d');
+     $base_date = date('Y-m-d', strtotime('+1 day '.$d));
+     $calendars = UserCalendar::where('trial_id', '>', 0)
+                    ->findStatuses(['new', 'confirm'])
+                    ->where('start_time', '<' ,$base_date.' 23:59:59')
+                    ->get();
+     foreach($calendars as $calendar){
+       echo 'calendar[id='.$calendar->id."][status=".$calendar->status."]\n";
+       if($calendar->status=='confirm'){
+         foreach($calendar->get_students() as $member){
+           $member->status_update('cancel', '未確認のまま時間経過によりキャンセル', 1);
+         }
+       }
+       foreach($calendar->get_teachers() as $member){
+         $member->status_update('cancel', '未確認のまま時間経過によりキャンセル', 1);
+       }
+     }
    }
    protected function send_slack($message, $msg_type, $username=null, $channel=null) {
      $controller = new Controller;
