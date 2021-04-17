f3fba79674323dc784e08474c4458d058e784673
osuke-kakuta <osuke.kakuta@gmail.com>
2021-04-17 16:41:48 +0900

    週の回数によるマッチング変更は見送り

--- app/Models/UserCalendarMemberSetting.php
+++ app/Models/UserCalendarMemberSetting.php
@@ -505,11 +505,10 @@ class UserCalendarMemberSetting extends UserCalendarMember
                               ->where('grade',$this->user->details()->get_tag_value('grade'))
                               ->where('course_type',$setting->get_tag_value('course_type'))
                               ->where('course_minutes',$setting['course_minutes'])
-                              ->where('lesson_week_count','<=',$this->user->get_enable_calendar_setting_count($setting->lesson(true)))
+                              ->where('lesson_week_count',$this->user->get_enable_calendar_setting_count($setting->lesson(true)))
                               ->where('is_exam',$this->user->details()->is_juken())
                               ->whereDate('start_date','<',$user_created_date)
-                              ->whereDate(DB::raw('IFNULL(end_date,"9999/12/31")'),'>',$user_created_date)
-                              ->orderBy('lesson_week_count','desc')->get();
+                              ->whereDate(DB::raw('IFNULL(end_date,"9999/12/31")'),'>',$user_created_date)->get();
       if($tuition_master->count() > 0){
         $tuition = $tuition_master->first()->tuition;
       }else{
