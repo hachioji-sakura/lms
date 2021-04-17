d14db7097406fedc3cb2fdd7fcfd74fa911c811b
osuke-kakuta <osuke.kakuta@gmail.com>
2021-04-17 16:43:49 +0900

    コミット漏れ

---
+++ config/attribute/order_status.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'new' => '下書き',
+  'fix' => '承認済',
+  'ordered' => '発注済',
+  'recieved' => '納品済',
+  'complete' => '受渡済',
+  'cancel' => 'キャンセル'
+];
+
+?>
---
+++ config/attribute/order_status_icon.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'new' => 'pen',
+  'fix' => 'check',
+  'ordered' => 'shopping-cart',
+  'recieved' => 'hands',
+  'complete' => 'thumbs-up',
+  'cancel' => 'ban'
+];
+
+?>
