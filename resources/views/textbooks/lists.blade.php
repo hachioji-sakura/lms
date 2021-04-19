2d8d849a187685e26b96b42d719be8932d537ee7
kasaibksn <kasaibksn@gmail.com>
2021-04-19 22:14:22 +0900

    #802 改めてselect系のコンポーネント化

--- resources/views/textbooks/lists.blade.php
+++ resources/views/textbooks/lists.blade.php
@@ -9,8 +9,8 @@
     @component('textbooks.forms.subject', ['prefix'=>'search_','textbook'=> $textbook??null,'subjects' => $subjects,'textbook_subjects' => $textbook->subject_list??null]); @endcomponent
     @component('textbooks.forms.grade', ['prefix'=>'search_','grades' => $grades,'textbook_grades' => $textbook->grade_list??null]); @endcomponent
     @component('textbooks.forms.select_difficulty', ['prefix'=>'search_','textbook' => $textbook??null ]); @endcomponent
-    @component('textbooks.forms.select_publisher', ['prefix'=>'search_','textbook'=> $textbook??null,'publishers' => $publishers]); @endcomponent
-    @component('textbooks.forms.select_supplier', ['prefix'=>'search_','textbook'=> $textbook??null,'suppliers' => $suppliers]); @endcomponent
+    @component('textbooks.select_form', ['prefix'=>'search_','textbook'=> $textbook??null,'collection' => $suppliers ,'target_item' =>'publisher']); @endcomponent
+    @component('textbooks.select_form', ['prefix'=>'search_','textbook'=> $textbook??null,'collection' => $suppliers ,'target_item' =>'supplier']); @endcomponent
     <div class="col-12 mb-2">
       <div class="form-group">
       <label for="search_keyword" class="w-100">
