e32c4915334db9bf3e84162260a354dabe0ca79d
osuke-kakuta <osuke.kakuta@gmail.com>
2021-05-14 14:25:44 +0900

    現存する未承認契約の処理をseederに追加

--- database/seeds/MakeAprilAgreementSeeder.php
+++ database/seeds/MakeAprilAgreementSeeder.php
@@ -14,9 +14,13 @@ class MakeAprilAgreementSeeder extends Seeder
     public function run()
     {
         DB::transaction(function(){
-            //TODO:askがない奴はcommitにする処理を入れる
-            
+            //体験フローの契約確認中のものを除いて、すべてをcommitに変更
+            $except_ids = Ask::where('type','agreement')->pluck('target_model_id');
+            Agreement::whereNotIn($except_ids)->where('status','new')->update(['status' => 'commit']);
+
+            //開始日がないなら4/1にセット
             Agreement::whereNull('start_date')->update(['start_date'=>"2021-04-01"]);
+            //存在する契約はすべて4月で終了するようにセット
             Agreement::all()->map(function($item){
                 return $item->update(['end_date'=>"2021-04-30"]);
             });
