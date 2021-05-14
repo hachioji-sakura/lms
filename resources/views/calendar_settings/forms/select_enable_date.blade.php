1b24c7be17319b3fa864aa6891aedb2bf4922e3d
hideo-yasui <yasumihosi@gmail.com>
2021-05-14 13:58:29 +0900

    修正

--- resources/views/calendar_settings/forms/select_enable_date.blade.php
+++ resources/views/calendar_settings/forms/select_enable_date.blade.php
@@ -5,7 +5,11 @@
   </label>
   <div class="input-group">
     <input type="text" name="enable_start_date" class="form-control float-left w-30" uitype="datepicker" placeholder="例：2000/01/01" required="true"
+    @if($_edit==false)
     minvalue="{{date("Y/m/01", strtotime('+1 month'))}}"
+    @elseif($_edit==true)
+    minvalue="{{$item->enable_start_date}}"
+    @endif
     @if(isset($item) && isset($item['enable_start_date']) && $item['enable_start_date']!='9999-12-31')
     value = "{{date('Y/m/d', strtotime($item['enable_start_date']))}}"
     @elseif(isset($item) && isset($item->schedule_start_hope_date))
