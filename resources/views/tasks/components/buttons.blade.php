8b34dec290f43b653577e27bc84663bf576b2112
kasaibksn <kasaibksn@gmail.com>
2021-06-07 11:52:55 +0900

    #1010 完了以外でもコピーできるよう修正

--- resources/views/tasks/components/buttons.blade.php
+++ resources/views/tasks/components/buttons.blade.php
@@ -47,8 +47,7 @@
     {{__('labels.review_button')}}
     </a>
   @endif
-  @if($item->status == "done")
-    <a href="javascript:void(0)" title="{{__('labels.copy_button')}}" page_form="dialog" page_title="{{$item->title}}" page_url="/tasks/create?copy_id={{$item->id}}&student_id={{$student->id}}" class="btn btn-sm btn-outline-{{$is_footer ? 'app' : 'primary'}} mr-1 mb-1" role="button">
-      {{__('labels.copy_button')}}
-    </a>
-  @endif
+  <a href="javascript:void(0)" title="{{__('labels.copy_button')}}" page_form="dialog" page_title="{{$item->title}}" page_url="/tasks/create?copy_id={{$item->id}}&student_id={{$student->id}}" class="btn btn-sm btn-outline-{{$is_footer ? 'app' : 'primary'}} mr-1 mb-1" role="button">
+    {{__('labels.copy_button')}}
+  </a>
+
